9d630e04fdefcf59b7ac5f4530c4d5b395c3e9c4
tigrankar23 <tigrankar23@gmail.com>
2024-04-30 15:26:36 +0400

    new

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup as BS
 url = 'https://rp5.am/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0%95%D1%80%D0%B5%D0%B2%D0%B0%D0%BD%D0%B5'
 class_name = 't_0'
 
+
+
 r = requests.get(url)
 html = BS(r.text, 'html.parser')
 t = html.find(class_ = class_name).text
